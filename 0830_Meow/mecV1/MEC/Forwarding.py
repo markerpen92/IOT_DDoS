c57a2a3c16e7a3f27dd4007528750bc2ae664c47
root <aa098933967901@gmail.com>
2024-04-02 18:25:27 +0800

    testing

--- 0830_Meow/mecV1/MEC/Forwarding.py
+++ 0830_Meow/mecV1/MEC/Forwarding.py
@@ -25,7 +25,6 @@ def append_string_to_file(input_string, filename) :
         with lock : 
             with open(filename, 'a+') as file :
                 input_string = input_string.replace('\r', '')   # Data processing(HTML)
-                input_string = input_string.replace('\t', '')   # Data processing(HTML)
                 input_string = input_string.replace('\n', '')   # Data processing(HTML) 
                 input_string = input_string.replace('\r\n', '') # Data processing(HTML)
                 input_string = input_string.replace('\n\r', '') # Data processing(HTML) 
