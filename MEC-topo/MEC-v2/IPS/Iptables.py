f1a99ebf892dff780796b88647da49e2e4330e90
markerpen92 <markchou920219@gmail.com>
2024-04-05 20:54:02 +0800

    MEC-topo

--- MEC-topo/MEC-v2/IPS/Iptables.py
+++ MEC-topo/MEC-v2/IPS/Iptables.py
@@ -1,3 +1,4 @@
+import re
 import os
 import time
 import threading
@@ -23,6 +24,7 @@ def ReadSuspiciousFile(filename) :
 
 
 def ReadOneRecord(filename , lineNum) : 
+    
     file_lock = threading.Lock()
     with file_lock : 
         with open(filename , 'r+') as file :
@@ -38,11 +40,12 @@ def ReadOneRecord(filename , lineNum) :
 
 
 def RemoveOneRecord(filename , lineNum) : 
+    print("start-1")
     file_lock = threading.Lock()
     with file_lock : 
         with open(filename , 'r+') as file :
             lines = file.readlines()
-            
+        
             if lines : 
                 line = lines.pop(lineNum)
                 file.seek(0)
@@ -67,63 +70,81 @@ def WriteRecordIntoFile(filename , record) :
 
 
 
-
-
-
-
 def Iptables(IOTDevicesInfo , BlockList) : 
     sus_filename = "IPS/SuspiciousList.txt"
     cln_filename = "IPS/CleanerList.txt"
-    # BadIP = ReadSuspiciousFile(filename)
+
     BadIP = RemoveOneRecord(sus_filename , 0)
     GoodIP = RemoveOneRecord(cln_filename , 0)
+
     if BadIP == None and GoodIP == None : 
         time.sleep(1.0)
-        return None
-    print(f"Ban Bad User : {BadIP}")
-    BlockList.append(BadIP)
-    print(IOTDevicesInfo[BadIP]['TrustValue'])
-    del IOTDevicesInfo[BadIP]
-    cmd = f'sudo iptables -t filter -I FORWARD -j DROP -s {BadIP}'
-    print(f"IOTDevicesInfo : {IOTDevicesInfo} || CMD : {cmd}")
-    os.system(cmd)
+        return None ,None
+
+    if BadIP != None : 
+        print(f"Ban Bad User : {BadIP}")
+        BlockList.append(BadIP)
+        if type(BadIP) == str : 
+            BadIP = BadIP.replace('\n','')  #fix IP./r error!
+        print(IOTDevicesInfo[BadIP]['TrustValue'])
+        del IOTDevicesInfo[BadIP]
+        cmd = f'sudo iptables -t filter -I FORWARD -j DROP -s {BadIP}'
+        print(f"IOTDevicesInfo : {IOTDevicesInfo} || CMD : {cmd}")
+        os.system(cmd)
+        
+    if GoodIP != None : 
+        GoodIP = GoodIP.replace('\n','')  #fix IP./r error!
 
     return BadIP , GoodIP 
 
 
 
+def GetRecordToTrain(BadIP=None , GoodIP=None):
+    GoodTargetIP =None 
+    BadTargetIP =None
+    BadRole = None
+    GoodRole = None
 
-
-
-
-
-def GetRecordToTrain(BadIP=None , GoodIP=None) : 
     if BadIP == None and GoodIP == None : 
+        print("retrunnnnn1")
         return
     elif BadIP != None : 
+        print(f'BadIP : {BadIP}')
         BadRole = 'BAD '
         BadTargetIP = BadIP
     elif GoodIP!= None :
+        print(f'GoodIP : {GoodIP}')
         GoodRole = 'GOOD '
         GoodTargetIP = GoodIP
 
-    filename = "IPS/record.txt"
-
+    filename = "./IPS/record.txt"
     line_num = 0
     OneRecord = ''
-    while OneRecord != None : 
-        OneRecord = ReadOneRecord(filename , line_num)
-        patterns = OneRecord.split(' ')
 
-        if BadTargetIP == patterns[0] : 
-            record = RemoveOneRecord(filename , line_num)
-            record = BadRole + record
-            TraingFile = 'IDS/TrainingList.txt'
-            WriteRecordIntoFile(TraingFile , record)
-        elif GoodTargetIP == patterns[0] :
-            record = RemoveOneRecord(filename , line_num)
-            record = GoodRole + record
-            TraingFile = 'IDS/TrainingList.txt'
-            WriteRecordIntoFile(TraingFile , record)
+    while OneRecord != None : 
+
+        try: 
+            OneRecord = ReadOneRecord(filename , line_num)
+            if OneRecord == None : 
+                return
+            patterns = OneRecord.split(' ')
+            
+            if  BadTargetIP == patterns[0]:
+                record = RemoveOneRecord(filename , line_num)
+                record = BadRole + record
+                TraingFile = 'IDS/TrainingList.txt'
+                WriteRecordIntoFile(TraingFile , record)
+
+            elif GoodTargetIP == patterns[0] :
+                record = RemoveOneRecord(filename , line_num)
+                record = GoodRole + record
+                TraingFile = 'IDS/TrainingList.txt'
+                WriteRecordIntoFile(TraingFile , record)
         
-        line_num += 1
+            line_num += 1
+            time.sleep(0.1)
+
+        except Exception as e :
+            print(f"<Error> GetRecordToTrain-While : {e}")
+            time.sleep(0.5)
+            return 
