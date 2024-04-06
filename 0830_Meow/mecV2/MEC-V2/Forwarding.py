b6dcbacbf0fe80f495718c0f975370a5eed9ba7b
user <aa098933967901@gmail.com>
2024-04-06 14:54:32 +0800

    meow3

--- 0830_Meow/mecV2/MEC-V2/Forwarding.py
+++ 0830_Meow/mecV2/MEC-V2/Forwarding.py
@@ -133,12 +133,14 @@ def packetParse(ThePacket , IOTDevicesInfo , BlockList) :
             TrafficInputstr = f"[Src IP]-{SrcIP}\t[Dst IP]-{DstIP}\t[Dstport]-{DstPort}\t[PktSize]-{len(PayloadData)}"
             CPUUseRateInputstr = f"[Src IP]-{SrcIP}\t[Dst IP]-{DstIP}\t[Dstport]-{DstPort}\t[ReplyRequest]-{ReplyRequest}"
             
-            encodeHTTPPayload = str(packet[TCP].payload)
-            print(f'TCP Paylaod: {encodeHTTPPayload}')
-          
-        
-            PacketFeatureInptstr = f"{SrcIP} {DstIP} {packet[TCP].window} {encodeHTTPPayload}"
+            # encodeHTTPPayload = str(packet[TCP].payload)
+            # print(f'TCP Paylaod: {encodeHTTPPayload}')
             
+        
+            # PacketFeatureInptstr = f"{SrcIP} {DstIP} {packet[TCP].window} {encodeHTTPPayload}"
+            PacketFeatureInptstr = packet[Raw].load
+            print(PacketFeatureInptstr)
+
             if SrcIP not in RS_IP :append_string_to_file(PacketFeatureInptstr, PacketFeatureRecord)
             append_string_to_file(ConnectedTimeInputstr , ConnectedTimeRecord)
             append_string_to_file(ConnectedTimeInputstr , ConnectedTimeRecord)
