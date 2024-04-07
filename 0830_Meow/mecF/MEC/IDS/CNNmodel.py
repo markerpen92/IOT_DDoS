61b96dfbba856f41a2ee41bfc82f78767bcd8f21
user <aa098933967901@gmail.com>
2024-04-07 23:41:04 +0800

    meow

--- 0830_Meow/mecF/MEC/IDS/CNNmodel.py
+++ 0830_Meow/mecF/MEC/IDS/CNNmodel.py
@@ -1,11 +1,11 @@
 import threading
+import os
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 # from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
 
 
 
@@ -21,13 +21,13 @@ class CNN_Model(nn.Module):
         '''--------------------------------CNNmodel Setting------------------------------------------------'''
 
         # Convolution 1 , Input Size : 1*pkt_features
-        self.cnn1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=(mask1,1), stride=1, padding=0) # Output Size : 16*(pkt_features-mask1+1)
+        self.cnn1 = nn.Conv2d(in_channels=4, out_channels=16, kernel_size=(1,mask1), stride=1, padding=0) # Output Size : 16*(pkt_features-mask1+1)
         self.relu1 = nn.ReLU() # activation
         # Max pool 1
         self.maxpool1 = nn.MaxPool2d(kernel_size=2 , padding=1) # Output Size : 16*(pkt_features-2)/2
 
         # Convolution 2 , Input Size : 16*(pkt_features-mask1+1)/2
-        self.cnn2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(mask2,1), stride=1, padding=0) # Output Size : 16*((pkt_features-mask1+1)/2)-mask2+1
+        self.cnn2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(1,mask2), stride=1, padding=0) # Output Size : 16*((pkt_features-mask1+1)/2)-mask2+1
         self.relu2 = nn.ReLU() # activation
         # Max pool 2
         self.maxpool2 = nn.MaxPool2d(kernel_size=2 , padding=1) # Output Size : 32*(((pkt_features-mask1+1)/2)-mask2+1)/2
@@ -47,7 +47,8 @@ class CNN_Model(nn.Module):
 
         '''------------------------------------------------------------------------------------------------------'''
 
-        self.LearningRate = 0.01
+        self.input_shape = (-1,4,1,1)
+        self.LearningRate = 0.9
         self.optimizer = torch.optim.Adam(self.parameters(), lr=self.LearningRate)
         self.LossFunction = nn.CrossEntropyLoss()
 
@@ -74,7 +75,6 @@ class CNN_Model(nn.Module):
         # Max pool 2
         out = self.maxpool2(out)
         out = out.view(out.size(0), -1)
-
         # Fully connected 1
         out = self.fc1(out)
         out = F.relu(out)
@@ -105,57 +105,85 @@ class CNN_Model(nn.Module):
 
 
         file_lock = threading.Lock()
+        
         TrainingFile = 'IDS/TrainingList.txt'
 
-        with file_lock : 
-            with open(TrainingFile , 'r+') as file : 
-                lines = file.readlines()
-                NewContext = []
+        if os.path.exists(TrainingFile) : 
+            with file_lock : 
+                with open(TrainingFile , 'r+') as file : 
+                    lines = file.readlines()
+                    NewContext = []
 
-                for line in lines : 
-                    patterns = line.split(' ')
+                    for line in lines : 
+                        patterns = line.split(' ')
 
-                    role = patterns[0]
+                        role = patterns[0]
 
-                    if role=='GOOD' : 
-                        self.DataLoader.append((patterns[1:]) , 0)
-                    elif role=='BAD' : 
-                        self.DataLoader.append((patterns[1:]) , 1)
+                        if role=='GOOD' : 
+                            self.DataLoader.append((patterns[1:]) , [0])
+                        elif role=='BAD' : 
+                            self.DataLoader.append((patterns[1:]) , [1])
 
-                file.write('')
-                file.close()
+                    file.write('')
+                    file.close()
 
 
-        data_loader = test_dataloader
         if test_dataloader == None : 
             data_loader = self.DataLoader
+        else : 
+            for training_data in test_dataloader : 
+                if training_data[0] == 'GOOD' : 
+                    self.DataLoader.append(((training_data[1:]) , [0]))
+                elif training_data[0] == 'BAD' : 
+                    self.DataLoader.append(((training_data[1:]) , [1]))
+            
+        data_loader = self.DataLoader
 
 
         for round in range(self.TrainingEpisodes) : 
+            total_train = correct_time = 0
+            his = []
+
             for i , (pkt , label) in enumerate(data_loader) : 
-                InputData = Variable(pkt)
+
+                pkt = np.array([np.array(item, dtype=np.float32) for item in pkt])
+                # pkt = [[item] for item in pkt]
+                
+                pkt_tensor = torch.tensor(pkt)
+                pkt_tensor = Variable(pkt_tensor.view(self.input_shape))
+
+                label = torch.tensor(label)
                 label = Variable(label)
 
                 self.optimizer.zero_grad()
 
-                output = self.forward(InputData)
+                output = self.forward(pkt_tensor)
 
-                train_loss = self.LossFunction(output , label)
+        
+
+                train_loss = self.LossFunction(output, label)
 
                 train_loss.backward()
 
                 self.optimizer.step()
 
-                predicted = torch.max(output.data , 1)[1]
+                predicted = torch.max(output.data, 1)[1]
+  
 
                 total_train += 1
-                correct_time += 1 if predicted==label else correct_time
+                if predicted == label : 
+                    correct_time += 1
+                    his.append(1)
+                else : 
+                    his.append(0)
 
-            train_accuracy = 100 * correct_time / float(total_train)
+
+            train_accuracy = 100 * correct_time / total_train
             TrainingAccuracy.append(train_accuracy)
             TrainingLoss.append(train_loss.data)
 
-            print(f'Round[{round}] - accu : {train_accuracy}\n')
+            if round%100==0 : 
+                print(f"Round<{round}> - accu : {train_accuracy}\n")
         
         self.SaveModel()
 
@@ -192,15 +220,22 @@ class CNN_Model(nn.Module):
                     predicted = torch.max(output.data , 1)[1]
 
                     if predicted : 
-                        print(srcip, 'is detected')
-                        '''
-                        if predicted is 1 -> Block the IP
-                        '''
-                    else : 
                         reocrdfile = 'IPS/record.txt'
                         with open(reocrdfile , 'a+')as file :
                             file.write(Input + '\n')
+                            
+                    else : 
+                        print(srcip, 'is detected')
+                        sus_file = 'IPS/suspicious.txt'
+                        susFile_lock = threading.Lock()
 
+                        with susFile_lock:
+                            with open(sus_file , 'a+')as file :
+                                file.write(srcip + '\n')
+                                file.close()
+                        '''
+                        if predicted is 1 -> Block the IP
+                        '''
 
                     NewContext.remove(idx)
 
